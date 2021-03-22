1f5513286b382f1724c87a792e80b4b823364acf
Shinichiro Oba <bricklife@gmail.com>
2021-03-22 14:49:25 +0900

    Update download.js

--- Scrub/download.js
+++ Scrub/download.js
@@ -1,4 +1,5 @@
 // ref: https://stackoverflow.com/questions/61702414/wkwebview-how-to-handle-blob-url/61703086
+// ref: https://luke-1220.github.io/post/webview-dl/
 
 (function () {
     document.querySelectorAll('a').forEach(async (el) => {
