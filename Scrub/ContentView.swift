28d2ce74ee5ae2f3972935ec6edfa583b3615a4a
Shinichiro Oba <bricklife@gmail.com>
2021-04-19 16:42:49 +0900

    Fix the preview

--- Scrub/ContentView.swift
+++ Scrub/ContentView.swift
@@ -18,5 +18,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(Preferences())
     }
 }
