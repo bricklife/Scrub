e649b1cc9b1afb188c5a476f8fba093aa4354dea
Shinichiro Oba <bricklife@gmail.com>
2021-04-23 20:05:05 +0900

    Add CircleProgressView

---
+++ Scrub/CircleProgressView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleProgressView.swift
+//  Scrub
+//
+//  Created by Shinichiro Oba on 2021/04/23.
+//
+
+import SwiftUI
+
+struct CircleProgressView : Shape {
+    
+    let progress: Double
+    
+    func path(in rect: CGRect) -> Path {
+        let r = min(rect.width, rect.height) / 2
+        
+        var p = Path()
+        
+        p.addArc(center: CGPoint(x: rect.midX, y:rect.midY),
+                 radius: r,
+                 startAngle: .degrees(-90),
+                 endAngle: .degrees(360 * progress - 90),
+                 clockwise: false)
+        
+        return p.strokedPath(.init(lineWidth: 1))
+    }
+}
