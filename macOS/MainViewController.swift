41e76d492369688e035ae941651d494d4372a086
Shinichiro Oba <bricklife@gmail.com>
2021-12-25 16:09:28 +0900

    Rename and use ScratchWebViewControllerDelegate

---
+++ macOS/MainViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MainViewController.swift
+//  Scrub
+//
+//  Created by Shinichiro Oba on 2021/12/24.
+//
+
+import AppKit
+import Combine
+import ScratchWebKit
+import ScratchLink
+
+class MainViewController: NSViewController {
+    
+    @IBOutlet weak var webViewController: ScratchWebViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webViewController = children.first as? ScratchWebViewController
+        webViewController?.delegate = self
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        
+        //let url = URL(string: "https://bricklife.com/scratch-gui/")!
+        let url = URL(string: "https://stretch3.github.io/")!
+        webViewController?.load(url: url)
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
+
+extension MainViewController: ScratchWebViewControllerDelegate {
+    func decidePolicyFor(url: URL, isScratchEditor: Bool, decisionHandler: @escaping (WebFilterPolicy) -> Void) {
+        print(#function, url, isScratchEditor)
+        decisionHandler(.allow)
+    }
+    
+    func didDownloadFile(at url: URL) {
+        print(#function, url)
+    }
+    
+    func didFail(error: Error) {
+        print(#function, error)
+    }
+    
+    func canStartScratchLinkSession(type: SessionType) -> Bool {
+        print(#function, type)
+        return true
+    }
+    
+    func didStartScratchLinkSession(type: SessionType) {
+        print(#function, type)
+    }
+    
+    func didFailStartingScratchLinkSession(type: SessionType, error: SessionError) {
+        print(#function, type, error)
+    }
+}
