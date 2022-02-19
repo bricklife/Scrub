a076ce99c418ede517fe105125e3c603e1b4b14a
Shinichiro Oba <bricklife@gmail.com>
2022-02-19 23:51:13 +0900

    Add PreferencesViewController

---
+++ macOS/PreferencesViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PreferencesViewController.swift
+//  Scrub
+//
+//  Created by Shinichiro Oba on 2022/02/19.
+//
+
+import Cocoa
+
+class PreferencesViewController: NSViewController {
+    
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var groupView: NSView!
+    
+    private var preferences: Preferences {
+        return AppDelegate.shared.preferences
+    }
+    
+    private let homeArray: [Preferences.Home] = [
+        .scratchHome,
+        .scratchEditor,
+        .scratchMyStuff,
+        .customUrl,
+    ]
+    
+    private var buttons: [NSButton] {
+        return groupView.subviews
+            .compactMap({ $0 as? NSButton })
+            .sorted(by: { $0.tag < $1.tag })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let home = preferences.home
+        let index = homeArray.firstIndex(of: home) ?? 1
+        buttons[index].state = .on
+        
+        textField.stringValue = preferences.customUrl
+    }
+    
+    @IBAction func selectHome(_ sender: NSButton) {
+        AppDelegate.shared.preferences.home = homeArray[sender.tag]
+    }
+}
