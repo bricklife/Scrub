9bce4f8e153a52175cdb1996f8d4a59382de1b9e
Shinichiro Oba <bricklife@gmail.com>
2024-02-10 20:33:01 +0900

    Show CircleProgressView on visionOS

--- Scrub/CircleProgressView.swift
+++ Scrub/CircleProgressView.swift
@@ -15,7 +15,11 @@ struct CircleProgressView: View {
         Circle()
             .inset(by: 1.5)
             .trim(from: 0.0, to: CGFloat(progress))
+#if os(visionOS)
+            .stroke(Color.accentColor, lineWidth: 2.0)
+#else
             .stroke(Color.accentColor, lineWidth: 1.0)
+#endif
             .rotationEffect(Angle(degrees: -90))
     }
 }
